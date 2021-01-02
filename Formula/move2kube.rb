190ec98ac7a1ce49ae666f686e7037546011db2a
HarikrishnanBalagopal <harikrishmenon@gmail.com>
2021-01-03 01:26:47 +0530

    feat: add workflows to build the bottles (#25)
    * feat: add workflows to build the bottles
    
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- Formula/move2kube.rb
+++ Formula/move2kube.rb
@@ -1,16 +1,16 @@
 class Move2kube < Formula
-  desc "Command-line tool that accelerates the process of re-platforming to Kubernetes/Openshift."
+  desc "Command-line tool that accelerates migration of your app to Kubernetes/Openshift"
   homepage "https://konveyor.io/move2kube"
   url "https://github.com/konveyor/move2kube/archive/v0.1.0.tar.gz"
   sha256 "b323c6045f4a970198021a8a31ac9ad9b17920d0f7a38c7bf23dbf6ee1c29c76"
   license "Apache-2.0"
 
   depends_on "go@1.15" => :build
-  depends_on "helm" => :optional
-  depends_on "docker" => :optional
-  depends_on "operator-sdk" => :optional
-  depends_on "kubernetes-cli" => :optional
   depends_on "buildpacks/tap/pack" => :optional
+  depends_on "docker" => :optional
+  depends_on "helm" => :optional
+  depends_on "kubernetes-cli" => :optional
+  depends_on "operator-sdk" => :optional
 
   def install
     system "make", "build"
@@ -18,7 +18,7 @@ class Move2kube < Formula
   end
 
   test do
-    want = 'v0.1.0'
+    want = "v0.1.0"
     assert_match want, shell_output("#{bin}/move2kube version")
   end
 end
