54b4a46d6a919ce9c8b6e0ac5146e94f2a080731
HarikrishnanBalagopal <20921177+HarikrishnanBalagopal@users.noreply.github.com>
2021-01-02 20:19:56 +0000

    move2kube: add 0.1.0 bottle.

--- Formula/move2kube.rb
+++ Formula/move2kube.rb
@@ -5,6 +5,13 @@ class Move2kube < Formula
   sha256 "b323c6045f4a970198021a8a31ac9ad9b17920d0f7a38c7bf23dbf6ee1c29c76"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/konveyor/homebrew-move2kube/releases/download/move2kube-0.1.0"
+    cellar :any_skip_relocation
+    sha256 "66df3fc240bd93909f561ec0ba0770106676723f13a79786bd66cd6348b66af3" => :catalina
+    sha256 "231b4c264047b1380c8ca659227216d9e74e88d732bffe5a48912499a3f09587" => :x86_64_linux
+  end
+
   depends_on "go@1.15" => :build
   depends_on "buildpacks/tap/pack" => :optional
   depends_on "docker" => :optional
