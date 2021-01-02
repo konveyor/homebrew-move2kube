328f7ec85e9869fa41e41039e5560c85e3eb13c1
HarikrishnanBalagopal <harikrishmenon@gmail.com>
2021-01-02 20:47:33 +0530

    fix: formula to build and test the project (#23)
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- Formula/move2kube.rb
+++ Formula/move2kube.rb
@@ -1,38 +1,29 @@
 class Move2kube < Formula
-  desc "Command-line tool that accelerates the process of re-platforming to Kubernetes/Openshift"
+  desc "Command-line tool that accelerates the process of re-platforming to Kubernetes/Openshift."
   homepage "https://konveyor.io/move2kube"
-  url "https://github.com/konveyor/move2kube.git",
-      tag:      "v0.1.0-alpha",
-      revision: "d4590d5693a570adc5d9faaaaf9bbc977a013e1f"
+  url "https://github.com/konveyor/move2kube/archive/v0.1.0.tar.gz"
+  sha256 "b323c6045f4a970198021a8a31ac9ad9b17920d0f7a38c7bf23dbf6ee1c29c76"
   license "Apache-2.0"
-  head "https://github.com/konveyor/move2kube.git"
 
-  bottle do
-    cellar :any_skip_relocation
-#    sha256 "" => :catalina
-#    sha256 "" => :mojave
-#    sha256 "" => :high_sierra
-  end
-
-  depends_on "go" => :build
+  depends_on "go@1.15" => :build
+  depends_on "helm" => :optional
+  depends_on "docker" => :optional
+  depends_on "operator-sdk" => :optional
+  depends_on "kubernetes-cli" => :optional
+  depends_on "buildpacks/tap/pack" => :optional
 
   def install
     system "make", "build"
     bin.install "bin/move2kube"
-
-    # mkdir "man1" do
-    #  system bin/"move2kube", "docs", "--type", "man"
-    #  man1.install Dir["*"]
-    # end
-
-    # output = Utils.safe_popen_read({ "SHELL" => "bash" }, bin/"move2kube", "completion", "bash")
-    # (bash_completion/"move2kube").write output
-
-    # output = Utils.safe_popen_read({ "SHELL" => "zsh" }, bin/"move2kube", "completion", "zsh")
-    # (zsh_completion/"_move2kube").write output
   end
 
   test do
-    system "#{bin}/move2kube"
+    want = 'version: v0.1.0
+gitCommit: 0e9d69d02c16379c6492e2404080fa295cc08a5f
+gitTreeState: clean
+goVersion: go1.15.2
+
+'
+    assert_match want, shell_output("#{bin}/move2kube version -l")
   end
 end
--- README.md
+++ README.md
@@ -1,11 +1,22 @@
-[![Build Status](https://travis-ci.com/konveyor/homebrew-move2kube.svg?branch=main)](https://travis-ci.com/konveyor/homebrew-move2kube)
 [![License](https://img.shields.io/:license-apache-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0.html)
 [![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/konveyor/homebrew-move2kube/pulls)
 [<img src="https://img.shields.io/badge/slack-konveyor/move2kube-green.svg?logo=slack">](https://kubernetes.slack.com/archives/CR85S82A2)
 
 # Move2Kube - Homebrew
 
-Brew formula for [Move2Kube](https://github.com/konveyor/move2kube).
+Brew formula for [Move2Kube](https://github.com/konveyor/move2kube)
+
+## Installation
+```
+brew tap konveyor/move2kube
+brew install move2kube
+```
+
+After installation run
+```
+move2kube version
+```
+to make sure it was installed properly.
 
 ## Discussion
 
