11fcf678959b3cf7627ccf718a1a7b5e97fb5d22
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2021-04-21 18:27:14 +0530

    fix: apply automated fix because for deprecations
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- Formula/move2kube.rb
+++ Formula/move2kube.rb
@@ -13,10 +13,9 @@ class Move2kube < Formula
 
   bottle do
     root_url "https://github.com/konveyor/homebrew-move2kube/releases/download/move2kube-0.1.0"
-    cellar :any_skip_relocation
-    sha256 "b5076d440d60e6fbf78a0e46700bb25fac3c8a72c8b8b705f17a8c27d76aaecb" => :big_sur
-    sha256 "9f3d4caacba9e2e55d06ee02a13f3efc73748090759e98f612bc2ae4484349a6" => :catalina
-    sha256 "f2758815ccbcf7d1465c06b8169cab2c62c15d7b862032fc9df1cda81a47f590" => :x86_64_linux
+    sha256 cellar: :any_skip_relocation, big_sur:      "b5076d440d60e6fbf78a0e46700bb25fac3c8a72c8b8b705f17a8c27d76aaecb"
+    sha256 cellar: :any_skip_relocation, catalina:     "9f3d4caacba9e2e55d06ee02a13f3efc73748090759e98f612bc2ae4484349a6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f2758815ccbcf7d1465c06b8169cab2c62c15d7b862032fc9df1cda81a47f590"
   end
 
   depends_on "go@1.15" => :build
