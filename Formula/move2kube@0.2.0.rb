a43df62f4bfa4ee08fedab7df089e2c865dbfaab
HarikrishnanBalagopal <harikrishmenon@gmail.com>
2021-10-18 02:26:15 +0530

    feat: create a homebrew release for every version (#36)
    create new formula file, still need to bottle correctly
    
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- Formula/move2kube@0.2.0.rb
+++ Formula/move2kube@0.2.0.rb
@@ -1,4 +1,4 @@
-class Move2kube < Formula
+class Move2kubeAT020 < Formula
   desc "Command-line tool that accelerates migration of your app to Kubernetes/Openshift"
   homepage "https://konveyor.io/move2kube"
   license "Apache-2.0"
@@ -9,12 +9,6 @@ class Move2kube < Formula
       using:    :git,
       tag:      "v0.2.0",
       revision: "8a4e2b89f0908e08fa817467bf4cecfcb9649cc9"
-  end
-
-  bottle do
-    root_url "https://github.com/konveyor/homebrew-move2kube/releases/download/v0.2.0"
-    sha256 cellar: :any_skip_relocation, big_sur:      "981479c94aaa11eaa74e88ef9edc8f1cc9bd42c683cf23bf0dcbe72e4229cf64"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "aaca8161fd8d3c251a969028e4dcbc66f203ea99b0fceb913167ae70f0301851"
   end
 
   depends_on "go@1.16" => :build
--- Formula/move2kube@0.3.0-alpha.3.rb
+++ Formula/move2kube@0.3.0-alpha.3.rb
@@ -1,4 +1,4 @@
-class Move2kube < Formula
+class Move2kubeAT030Alpha3 < Formula
   desc "Command-line tool that accelerates migration of your app to Kubernetes/Openshift"
   homepage "https://konveyor.io/move2kube"
   license "Apache-2.0"
@@ -7,14 +7,8 @@ class Move2kube < Formula
   stable do
     url "https://github.com/konveyor/move2kube.git",
       using:    :git,
-      tag:      "v0.2.0",
-      revision: "8a4e2b89f0908e08fa817467bf4cecfcb9649cc9"
-  end
-
-  bottle do
-    root_url "https://github.com/konveyor/homebrew-move2kube/releases/download/v0.2.0"
-    sha256 cellar: :any_skip_relocation, big_sur:      "981479c94aaa11eaa74e88ef9edc8f1cc9bd42c683cf23bf0dcbe72e4229cf64"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "aaca8161fd8d3c251a969028e4dcbc66f203ea99b0fceb913167ae70f0301851"
+      tag:      "v0.3.0-alpha.3",
+      revision: "f22dd840a9bdf28bb1aac8e8411344a83f619751"
   end
 
   depends_on "go@1.16" => :build
