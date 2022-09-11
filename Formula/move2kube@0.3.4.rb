d1e44b1c75fa32180fb365de46db9e1eb6b7e045
move2kube <move2kube@gmail.com>
2022-09-11 12:38:59 +0000

    chore: update formula for new release. run_id 3031802718

--- Formula/move2kube@0.3.4.rb
+++ Formula/move2kube@0.3.4.rb
@@ -1,4 +1,4 @@
-class Move2kube < Formula
+class Move2kubeAT034 < Formula
   desc "Command-line tool that accelerates migration of your app to Kubernetes/Openshift"
   homepage "https://move2kube.konveyor.io/"
   license "Apache-2.0"
@@ -13,8 +13,8 @@ class Move2kube < Formula
 
   bottle do
     root_url "https://github.com/konveyor/homebrew-move2kube/releases/download/v0.3.4"
-    sha256 cellar: :any_skip_relocation, big_sur:      "fbf2348782367e519c8aac79d0d5515fd264ba8d3a0bf64c3b3aa54519039551"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "925a8b5a9757f26cf1d354b24e9601448e8f6fed43210817e2de7f27dbf0f420"
+    sha256 cellar: :any_skip_relocation, big_sur:      "eb305ec370376ec5bfb2d7fbfa0512b6faeb057ab15a347ca84c61b26893ce5a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3be29d999afe9e47e45ec37e29a4f8bc98262d9603d10b8dbd0294e9168415c8"
   end
 
   depends_on "go@1.18" => :build
